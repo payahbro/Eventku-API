cdf8704a6fd1c66156c4433036ed7d979ad7d816
payahbro <payah45678@gmail.com>
2025-12-28 21:34:46 +0700

    add route for migration testing

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,20 @@ use App\Http\Controllers\BookingController;
 use App\Http\Controllers\MidtransCallbackController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Http\Request;
+
+// Sekedar debug
+Route::get('/setup-database-darurat', function () {
+    try {
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return '<h1>SUKSES! Database berhasil di-reset & di-migrate.</h1><br>' . nl2br(Artisan::output());
+    } catch (\Exception $e) {
+        return '<h1>GAGAL!</h1><br>Error: ' . $e->getMessage();
+    }
+});
+
+
 
 // Auth
 Route::post('/register', [AuthController::class, 'register']);
