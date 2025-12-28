bcb3a07f4e0e90b27a59936b2007a14beb974cb1
payahbro <payah45678@gmail.com>
2025-12-28 21:58:06 +0700

    fix routes web

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,16 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-// Sekedar debug
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/setup-database-darurat', function () {
     try {
+        // Kita coba wipe bersih dulu (karena lo tadi udah drop schema)
+        // lalu migrate ulang
         Artisan::call('migrate:fresh', ['--force' => true]);
+
         return '<h1>SUKSES! Database berhasil di-reset & di-migrate.</h1><br>' . nl2br(Artisan::output());
     } catch (\Exception $e) {
         return '<h1>GAGAL!</h1><br>Error: ' . $e->getMessage();
